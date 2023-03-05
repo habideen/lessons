d63f774873f16f8e1a39ccdc0b665797c749fa0f
Habideen Ibrahim <habideenibrahim@gmail.com>
2023-03-05 18:29:41 +0100

    Create a method for each object property

--- javascript/linkedIn/Become A Javascript Developer/Javascript Essential Training/Exercises/Practice/03_09/script.js
+++ javascript/linkedIn/Become A Javascript Developer/Javascript Essential Training/Exercises/Practice/03_09/script.js
@@ -12,15 +12,53 @@ const backpack = {
   volume: 30,
   color: "grey",
   pocketNum: 15,
+  lidOpen: true,
   strapLength: {
     left: 26,
     right: 26,
   },
+  newName: function (name) {
+    this.name = name;
+    console.info("Name changed to ", name);
+  },
+  newVolume: function (volume) {
+    this.volume = volume;
+    console.info("Volume changed to ", volume);
+  },
+  newColor: function (color) {
+    this.color = color;
+    console.info("Color changed to ", color);
+  },
+  newPocketNum: function (pocketNum) {
+    this.pocketNum = pocketNum;
+    console.info("Pocket number changed to ", pocketNum);
+  },
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
+    console.info("Lid status changed to ", lidStatus);
   },
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
 };
+
+console.log(backpack);
+
+backpack.toggleLid(false);
+console.log(backpack);
+
+backpack.newName("Miky Mouse");
+console.log(backpack);
+
+backpack.newPocketNum(23);
+console.log(backpack);
+
+backpack.newColor("Red");
+console.log(backpack);
+
+backpack.newVolume(19);
+console.log(backpack);
+
+backpack.newStrapLength(32, 17);
+console.log(backpack);
