09c4ccb047a8429c6a47a8dd282a0aff1ce95690
Habideen Ibrahim <habideenibrahim@gmail.com>
2023-03-23 17:36:58 +0100

    Traversing the DOM using JavaScript

--- javascript/linkedIn/Become A Javascript Developer/Javascript Essential Training/Exercises/Practice/05_04/script.js
+++ javascript/linkedIn/Become A Javascript Developer/Javascript Essential Training/Exercises/Practice/05_04/script.js
@@ -2,3 +2,35 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+//target first backback article element
+function updateLength(pack, side) {
+  const everydayLeftLen = document.querySelector(`#${pack} .${side}length`);
+  everydayLeftLen.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const inputEle = document.querySelector(
+      `#${pack} .${side}length input[name='${side}Length']`
+    );
+
+    const updateEle = document.querySelector(
+      `#${pack} .backpack__features .backpack__strap[data-side='${side}'] span`
+    );
+
+    if (inputEle.value != "" && inputEle.value > 0) {
+      updateEle.innerHTML = inputEle.value + " inches";
+    } else {
+      updateEle.innerHTML = "invalid";
+    }
+    inputEle.value = "";
+  });
+}
+
+updateLength("pack01", "left");
+updateLength("pack01", "right");
+
+updateLength("pack02", "left");
+updateLength("pack02", "right");
+
+//update site title
+document.querySelector("header .site-title").style.color = "Indigo";
